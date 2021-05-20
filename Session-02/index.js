5150146fb52521a1455c8462619620a17ab4992e
amirholyanasabnarab <amirholyanasabnarab@gmail.com>
2021-05-20 16:23:33 +0430

    Sessopn-02

--- Session-02/index.js
+++ Session-02/index.js
@@ -3,3 +3,26 @@ console.log('Wellcome to JavaScript')
 //Variable
 var myName = 'Gucci'
 console.log(typeof(myName))
+console.log('----------------------------')
+console.log(myName[2])
+console.log(myName[0])
+console.log(myName[myName.length-1])
+var size = myName.length
+console.log(size)
+petName = "گوچی"
+console.log(petName[0])
+console.log(petName.length)
+var test1 = "It's a book"
+console.log(test1)
+var test2 = 'این دابل کوتیشن " است'
+console.log(test2)
+test2 = "String in JavaScripti is Immutable"
+console.log(test2)
+var test3 = 'A\'li'
+console.log(test3)
+var test4 = 'Who is my love ?\n Gucci'
+console.log(test4)
+var test5 ="My name is\tAmirhossein\t\tMy family is\tOlyanasab"
+console.log(test5)
+var test6 = "My name is Amirhossein\\My family is Olyanasab"
+console.log(test6)
