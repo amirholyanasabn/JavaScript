c252335d8166c1b4883e33603156c30603049da4
amirholyanasabnarab <amirholyanasabnarab@gmail.com>
2021-05-24 14:26:01 +0430

    Session-03

---
+++ Session-03/index.js
@@ -0,0 +1,60 @@
+var a = 'amir'
+var b = 'hossein'
+var res = a+b
+console.log(res)
+var n = 'Sepideh'
+var age = 30
+var res1 = n + age
+console.log(typeof(res1))
+console.log(res1)
+console.log(5 + age + n)
+console.log(true + n)
+console.log(true + true + n)
+console.log('13'+65)
+console.log(5-3)
+console.log(2-5)
+console.log('foo'-3)
+console.log(5-'foo')
+console.log('ali'-'reza') //NaN
+console.log('100'-10)
+console.log(90-'30')
+console.log('amir'+' '+'is '+'34')
+console.log(2/-0)
+console.log(2/0)
+console.log(2*2)
+console.log(2*-2)
+console.log(-5*2)
+console.log(Infinity*0)
+console.log(Infinity*Infinity)
+console.log(Infinity*-Infinity)
+console.log('john'*2)
+console.log('2'*5)
+var x = 3
+var y = --x
+//var y = x--
+//var y = x++
+//var y = ++x
+console.log(x,y)
+var result=("b"+"a"+ +"a"+"a").toLocaleLowerCase
+console.log(result)
+
+
+
+
+// var dog = {
+//     name : 'gucci',
+//     ownerInfo : {
+//         owenerName : 'Sepideh',
+//         age : 30 ,
+//         job : 'Nurse' ,
+//         phone : '098912300000000',
+//         live : 'Tehran'
+//     },
+//     age : 5 ,
+//     color : 'Brown',
+// }
+
+
+// console.log(dog)
+// console.log(dog.ownerInfo.owenerName)
+// console.log(dog['ownerInfo']['job'])
