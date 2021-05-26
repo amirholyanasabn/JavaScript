42ba47af9f6838ecc5245463d00b684e4e6139ef
amirholyanasabnarab <amirholyanasabnarab@gmail.com>
2021-05-26 19:22:01 +0430

    session_03

---
+++ Session-03/exercise/index.js
@@ -0,0 +1,4 @@
+var firstName = 'Ali', lastName = 'Mousavi', age =31 , city = 'Tehran'
+console.log('My name is '+firstName+' '+lastName+'\n\t=>I have '+age+' years old'+"\n\t=>I'm living in "+city)
+var firstName = 'Amirhossein' , lastName = 'Olyanasab', age = 34 , city = 'Tehran'
+console.log('My name is '+firstName+' '+lastName+'\n\t=>I have '+age+' years old'+'\n\t=>I\'m living in '+city)
---
+++ Session-03/index4.js
@@ -0,0 +1,4 @@
+var a = !! 10
+console.log(a)
+var b = !!0
+console.log(b)
