aa2509fd2678258a0c27d3fa10e1e49290d7ccb4
amirholyanasabnarab <amirholyanasabnarab@gmail.com>
2021-05-30 14:58:13 +0430

    Session-04

---
+++ Session-04/function.js
@@ -0,0 +1,30 @@
+var myFunc = function(firstName , lastName){
+    return firstName+' '+lastName
+}
+
+console.log( myFunc('Amirhossein','Olyanasab'))
+
+function getFullName(firstName,lastName){
+    return firstName+' '+lastName
+}
+
+console.log(getFullName('Ali','Daei'))
+
+
+function sum(firstNum,secondNum){
+    return firstNum+secondNum
+}
+console.log(sum(12,8))
+console.log(sum(10,30))
+
+function func(){
+    return 'Gucci'
+}
+console.log(func())
+// var myVar = func()
+// console.log(myVar)
+
+function sayHello(){
+    console.log('Hello . . .')
+}
+sayHello()
