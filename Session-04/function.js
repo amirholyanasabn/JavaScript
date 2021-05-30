0064e96c72fdc26789a181f5a4aa82b93a463930
amirholyanasabnarab <amirholyanasabnarab@gmail.com>
2021-05-30 18:15:48 +0430

    Session-04

--- Session-04/function.js
+++ Session-04/function.js
@@ -11,11 +11,11 @@ function getFullName(firstName,lastName){
 console.log(getFullName('Ali','Daei'))
 
 
-function sum(firstNum,secondNum){
-    return firstNum+secondNum
-}
-console.log(sum(12,8))
-console.log(sum(10,30))
+// function sum(firstNum,secondNum){
+//     return firstNum+secondNum
+// }
+// console.log(sum(12,8))
+// console.log(sum(10,30))
 
 function func(){
     return 'Gucci'
@@ -28,3 +28,41 @@ function sayHello(){
     console.log('Hello . . .')
 }
 sayHello()
+
+//Anonymous Function
+//Call back
+
+// var anon = function(){
+//     alert('Hello JavaScript')
+// }
+// anon()
+
+function sum(a, b, c, d){
+   
+    if (!c)  c=0
+    // if (c===undefined) 
+    //      c=0
+    // if (d===undefined)
+    //     d=0 
+    d = d || 0
+    var result = a+b+c+d
+    return result
+}
+
+function sub(a, b){
+    return a-b
+}
+
+function operate(a, b, callback){
+    return callback(a, b)
+}
+
+console.log(operate(5,5,sum))
+console.log(operate(10,8,sub))
+console.log(operate(4,5,function(a,b){return a*b}))
+
+
+console.log(sum(2,5,3,5))
+console.log(sum(2,5,3))
+console.log(sum(2,5, undefined,5))
+console.log(sum(2,5,))
