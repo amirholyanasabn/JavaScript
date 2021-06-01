e37e7c9e8088d22ec33ca381c62be1eddc40e8be
amirholyanasabnarab <amirholyanasabnarab@gmail.com>
2021-06-01 17:54:49 +0430

    Session-06

---
+++ Session-06/index.js
@@ -0,0 +1,54 @@
+function Person(firstName,lastName,age,gender,father){
+    this.firstName = firstName
+    this.lastName = lastName
+    this.fullName = firstName + ' ' +lastName
+    this.age = age
+    this.gender = gender ? 'male' : 'female'
+    this.family = {
+        father : father
+    }
+}
+
+
+var gucciolyanasab = new Person('gucci','olyanasab','5',false,'amirhossein')
+console.log(gucciolyanasab)
+console.log(gucciolyanasab.firstName)
+console.log(gucciolyanasab.fullName)
+
+var amirhosseinOlyanasab = new Person('Amirhossein','Olyanasab',31,true,'Majid')
+console.log(amirhosseinOlyanasab.family.father)
+console.log(amirhosseinOlyanasab.gender)
+
+var sepideholyanasab = new Person('Sepideh','Olyanasab',31,false,'Majid')
+console.log(sepideholyanasab.firstName)
+
+
+// var amirhosseinOlyanasab = {
+//     firstName : 'Amirhossein',
+//     lastName : 'Olyanasab',
+//     age : 34,
+//     gender : true,
+//     family : {
+//         father : 'Majid'
+//     }
+// }
+
+// var gucciolyanasab = {
+//     firstName : 'Gucci',
+//     lastName : 'Olyanasab',
+//     age : 5,
+//     gender : false,
+//     family:{
+//         father : 'Amirhossein'
+//     }
+// }
+
+// var sepideholyanasab = {
+//     firstName : 'Sepideh',
+//     lastName : 'Olyanasab',
+//     age : 33,
+//     gender : false,
+//     family :{
+//         father : 'Majid'
+//     }
+// }
