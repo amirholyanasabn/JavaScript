3c4de54ee39b8ecaa1e58f819d0315118bc55a02
amirholyanasabnarab <amirholyanasabnarab@gmail.com>
2021-06-02 18:42:16 +0430

    Session-06

--- Session-06/thirdExerecise.js
+++ Session-06/thirdExerecise.js
@@ -43,18 +43,63 @@ console.log('Sub => ',subs(20,10,10))
 console.log('mul => ',mul(5,5,10))
 console.log('div => ',div(100,2,5,2))
 
-// function operation(){
-//     var numbers = []
-//     for(var i =1;i<arguments.length;i++){
-//         numbers[i-1] = arguments[i]
-//     }
+function operation(){
+    var numbers = []
+    for(var i =1;i<arguments.length;i++){
+        numbers[i-1] = arguments[i]
+    }
 
 
-//     switch(arguments[0]){
-//         case 'sum':
-//             return sum(numbers)
-//             break 
-//             case 'sub'
-//     }
-// }
-// console.log(operation('sum',12,10))
+    switch(arguments[0]){
+        case 'sum':
+            return sum.apply({},numbers)
+        case 'sub':
+            return subs.apply({},numbers)    
+        case 'mul':
+            return mul.apply(null,numbers)
+        case 'div' :
+            return div.apply(null,numbers)
+        default :
+            console.log('Operator not valid...')
+    }
+}
+console.log(operation('sum',12,10))
+console.log(operation('sub',100,99))
+console.log(operation('mul',4,5))
+console.log(operation('div',80,10))
+function func(param1,param2){
+    console.log(param1)
+    console.log(param2)
+    console.log(this.name)
+}
+var obj = {
+    name : 'Sepideh',
+    family : 'Olyanasab'
+}
+
+func.call(obj,'Nurse','Tehran')
+
+function welcome (greeting){
+    console.log(greeting+' '+this.name+' '+this.family)
+}
+var firstPerson = {
+    name : 'Amirhossein',
+    family : 'Olyanasab'
+}
+var secondPerson = {
+    name : 'سپیده',
+    family : 'علیانسب'
+}
+var thirdPerson = {
+    name : 'Gucci',
+    family : 'Olyanasab'
+}
+
+welcome.call(firstPerson,'Hallo')
+welcome.call(secondPerson,'سلام')
+welcome.apply(thirdPerson,['Hello pretty'])
+
+var numbers = [2,10,99,100,1,25]
+// var max = Math.max.apply({},numbers)
+var max = Math.max.apply(null,numbers)
+console.log(max)
