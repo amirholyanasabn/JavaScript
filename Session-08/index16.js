4e6877466228ee66953bd8f4085819b36bc843e1
amirholyanasabnarab <amirholyanasabnarab@gmail.com>
2021-06-16 19:00:56 +0430

    Session-08

---
+++ Session-08/index16.js
@@ -0,0 +1,7 @@
+var d = new Date
+console.log(d)
+var dDetail = d.getTime()
+console.log(dDetail)
+console.log(Date.parse('Wed Jun 16 2021 18:44:04 GMT+0430 '))
+console.log(Date.UTC(2020, 1, 2))
+console.log(Date.now())
